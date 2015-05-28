ab88dfb0d4da0d9c7a60bc5eebd629d60b78160e
yuheng.wpf <yuheng.wpf@alibaba-inc.com>
2015-05-28 20:33:48 +0800

    add files by wangpengfei

--- www/resource/js/course/view.js
+++ www/resource/js/course/view.js
@@ -1,22 +1,15 @@
 define(function(require, exports, moudle) {
+  var login = require('../index.js');
   $(function(){
-    $('#test_vedio').on('click',function(e){
-      var url = $(e.target).attr('data-src');
-      $("#video_container").css({
-        position : 'absolute',
-        top : "150px",
-        left : "150px",
-        width : '600px',
-        height : "338px"
-      }).html5video({
-        width : 600,
-        height : 338,
-        src : url ,
-        loop : false,
-        preload:true,
-        notsuportmsg:"您的浏览器不支持html5，无法使用该插件!"
+    if($('#signin_user')[0]){
+      var url = $('#start_learn').attr('data-href');
+      $('#start_learn').attr({
+        href : url
       })
-    })
-
+    }else{
+      $('#start_learn').on('click', function(e){
+        login.signin();
+      })
+    }
   })
 })
